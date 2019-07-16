7b3e8786e05488caffacb12c56ed1a093b5db1dc
Alyssa Nicoll <alyssaamnell@gmail.com>
2019-07-16 11:03:46 -0400

    Starter app with lazy loading routes

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,20 @@
+import { NgModule, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VideoDashboardComponent } from './video-dashboard/video-dashboard.component';
+import { VideoListComponent } from './video-list/video-list.component';
+import { VideoPlayerComponent } from './video-player/video-player.component';
+import { StatFiltersComponent } from './stat-filters/stat-filters.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: VideoDashboardComponent, pathMatch: 'full' },
+];
+
+@NgModule({
+  declarations: [VideoDashboardComponent, VideoListComponent, VideoPlayerComponent, StatFiltersComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+  ]
+})
+export class DashboardModule { }
